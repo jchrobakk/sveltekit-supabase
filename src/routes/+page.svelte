6c3f2662bd2d6e5e5cc7d53946a4a9a245f2fdcd
Jakub <jchrobakk@gmail.com>
2024-11-04 22:22:35 +0100

    add auth by email and oauth by github

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let { data } = $props();
+
+	const logout = async () => {
+		await data.supabase.auth.signOut();
+	};
+</script>
+
+<h1>Welcome to Supabase!</h1>
+{#if data.session}
+	<p>You are logged as {data.session.user.email}</p>
+	<button onclick={logout}>logout </button>
+{:else}
+	<p>You are not logged in</p>
+	<a href="/auth">login</a>
+{/if}
